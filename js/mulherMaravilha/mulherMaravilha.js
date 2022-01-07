2201d14f9bf0b741cc05f34e29da9fc42d13d06f
Hadassa Moraes <had.moraes@gmail.com>
2022-01-07 19:03:42 -0300

    add aparecePrompt em mulherMaravilha.js

--- js/mulherMaravilha/mulherMaravilha.js
+++ js/mulherMaravilha/mulherMaravilha.js
@@ -1,15 +1,12 @@
-var escolha = prompt('Digite A ou B');
-
 // Variáveis da Primeira Fase
 var botaoPrimeiraFase = '#botaoPrimeiraFase';
 var paginaAPrimeiraFase = '../../html/mulherMaravilha/mulherMaravilhaPrimeiroGameOver.html'
 var paginaBPrimeiraFase = '../../html/mulherMaravilha/mulherMaravilhaSegundaFase.html'
+var escolha;
 
-
-
-while(escolha != 'A' && escolha != 'B' && escolha!= 'a' && escolha!= 'b'){
-    alert('Resposta inválida, digite apenas A ou B');
+function aparecePrompt (){
     escolha = prompt('Digite A ou B');
+    return escolha
 }
 
 function usuarioEscolhe(paginaA, paginaB){
@@ -22,8 +19,10 @@ function usuarioEscolhe(paginaA, paginaB){
 
 // Executando Funções na Primeira Fase
 botaoPrimeiraFase.addEventListener('click', function(event){
+    aparecePrompt()
+    while(escolha != 'A' && escolha != 'B' && escolha!= 'a' && escolha!= 'b'){
+        alert('Resposta inválida, digite apenas A ou B');
+        escolha = prompt('Digite A ou B');
+    }
     usuarioEscolhe(paginaA, paginaB)
 })
-
-
-
