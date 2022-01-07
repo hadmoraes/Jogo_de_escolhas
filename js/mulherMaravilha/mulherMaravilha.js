10122cde1af545dd9b4148e824dd0e14a8b3486a
Hadassa Moraes <had.moraes@gmail.com>
2022-01-07 18:54:03 -0300

    add funcao UsuarioEscolhe e declara variaveis primeira Fase

--- js/mulherMaravilha/mulherMaravilha.js
+++ js/mulherMaravilha/mulherMaravilha.js
@@ -1,6 +1,29 @@
 var escolha = prompt('Digite A ou B');
 
+// Variáveis da Primeira Fase
+var botaoPrimeiraFase = '#botaoPrimeiraFase';
+var paginaAPrimeiraFase = '../../html/mulherMaravilha/mulherMaravilhaPrimeiroGameOver.html'
+var paginaBPrimeiraFase = '../../html/mulherMaravilha/mulherMaravilhaSegundaFase.html'
+
+
+
 while(escolha != 'A' && escolha != 'B' && escolha!= 'a' && escolha!= 'b'){
     alert('Resposta inválida, digite apenas A ou B');
     escolha = prompt('Digite A ou B');
 }
+
+function usuarioEscolhe(paginaA, paginaB){
+    if(escolha == "A" || 'a'){
+        window.open(paginaA);
+    } else{
+        window.open(paginaB);
+    }
+}
+
+// Executando Funções na Primeira Fase
+botaoPrimeiraFase.addEventListener('click', function(event){
+    usuarioEscolhe(paginaA, paginaB)
+})
+
+
+
