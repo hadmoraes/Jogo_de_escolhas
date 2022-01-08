db393b5f9053d75f493b3b04b94a6685dc5767b7
Hadassa Moraes <had.moraes@gmail.com>
2022-01-08 19:38:59 -0300

    add batmanFase1.js e colocou caminho no batmanFase1.html

---
+++ js/batman/batmanFase1.js
@@ -0,0 +1,40 @@
+//Variáveis comuns a todos
+var escolha;
+
+// Variáveis da Primeira Fase
+var botaoFase1 = document.querySelector('#botaoFase1');
+var pagAFase1 = '../../html/batman/batmanFase2.html' ;
+var pagBFase1 = '../../html/batman/batmanGameOver1.html';
+
+
+//Declarando funções
+
+function aparecePrompt(){
+    escolha = prompt('Digite A ou B');
+    validaEscolha(escolha);
+    return escolha;
+}
+
+function validaEscolha(escolha){
+    while(escolha != 'A' && escolha != 'B' && escolha!= 'a' && escolha!= 'b'){
+        alert('Resposta inválida, digite apenas A ou B');
+        escolha = prompt('Digite A ou B');
+    }
+}
+
+function abrePagConformeEscolha(paginaA, paginaB, escolha){
+    if(escolha == "A"){
+        window.open(paginaA,"_self");
+    } else if(escolha == "a"){
+        window.open(paginaA,"_self");
+    } else{
+        window.open(paginaB,"_self");
+    }
+}
+
+// Executando Funções na Primeira Fase
+botaoFase1.addEventListener('click', function(event){
+    aparecePrompt();
+    abrePagConformeEscolha(pagAFase1, pagBFase1, escolha);
+    botaoFase1.removeEventListener('click', this);
+})
