d811773938554ca928abdc6a81fdc24fb0cc1a47
Hadassa Moraes <had.moraes@gmail.com>
2022-01-09 12:42:09 -0300

    add superHomemFase2.html funcionando

---
+++ js/superHomem/superHomemFase2.js
@@ -0,0 +1,39 @@
+//Variáveis comuns a todos
+var escolha;
+
+// Variáveis da Segunda Fase
+var botaoFase2 = document.querySelector('#botaoFase2');
+var pagAFase2 = '../../html/superHomem/superHomemGameOver2.html' ;
+var pagBFase2 = '../../html/superHomem/superHomemFase3.html' ;
+
+
+//Declarando funções
+
+function aparecePrompt(){
+    escolha = prompt('Digite A ou B');
+    validaEscolha(escolha);
+    return escolha;
+}
+
+function validaEscolha(escolha){
+    while(escolha != 'A' && escolha != 'B' && escolha!= 'a' && escolha!= 'b'){
+        alert('Resposta inválida, digite apenas A ou B');
+        escolha = prompt('Digite A ou B');
+    }
+}
+
+function abrePagConformeEscolha(paginaA, paginaB, escolha){
+    if(escolha == "A"){
+        window.open(paginaA,"_self");
+    } else if(escolha == "a"){
+        window.open(paginaA,"_self");
+    } else{
+        window.open(paginaB,"_self");
+    }
+}
+
+// Executando Funções na Primeira Fase
+botaoFase2.addEventListener('click', function(event){
+    aparecePrompt();
+    abrePagConformeEscolha(pagAFase2, pagBFase2, escolha);
+})
